da4cc4cc0c26410b60fbe57b926abc84962efaca
d.nareyko <d.nareyko@itransition.com>
2024-01-03 18:07:10 +0000

    Initial Commit

---
+++ DynamicAppIcon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicAppIcon
+//
+//  Created by Nareyko, Diana on 3.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
